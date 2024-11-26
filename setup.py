11c6645e097626227729b9a981d14d427c3260ee
Qiyu Zhang <148504334+Qiyu-Zh@users.noreply.github.com>
2024-11-26 15:36:36 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name='PerfusionImaging',
+    version="0.1.0",
+    description="A short description of your package",
+    packages=find_packages(),  # Automatically find and include your package
+    install_requires=[         # List of dependencies
+      "SimpleITK",
+      "ipywidgets",
+      "IPython",
+      "scipy",
+      "matplotlib",
+      "numpy",
+      "pydicom"
+    ],
+    classifiers=[              # Metadata
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.8',
+)
